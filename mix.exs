ef8271bbff736d7844914d6e62d37db8a7211fcb
Jakub Migdał <jakub.migdal@swmansion.com>
2026-01-12 11:09:56 +0100

    Add bumblebee deps

--- mix.exs
+++ mix.exs
@@ -26,11 +26,7 @@ defmodule Membrane.Template.Mixfile do
     ]
   end
 
-  def application do
-    [
-      extra_applications: []
-    ]
-  end
+  def application, do: []
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_env), do: ["lib"]
@@ -39,6 +35,8 @@ defmodule Membrane.Template.Mixfile do
     [
       {:membrane_core, "~> 1.0"},
       {:membrane_raw_audio_format, "~> 0.12.0"},
+      {:bumblebee, "~> 0.6.0"},
+      {:exla, ">= 0.0.0"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, ">= 0.0.0", only: :dev, runtime: false},
