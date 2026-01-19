bfa0fd5ce97b7a9699641110cb6fe38f12152189
Jakub Migdał <jakub.migdal@swmansion.com>
2026-01-19 14:24:26 +0100

    Check for TranscriptEvent in integration test

--- test/integration/dummy_test.exs
+++ test/integration/dummy_test.exs
@@ -32,7 +32,7 @@ defmodule Membrane.Whisper.Integration.DummyTest do
   @input_file "test/fixtures/sherlock_1min.raw"
   @output_file "test/fixtures/output.raw"
 
-  test "dummy test" do
+  test "Audio buffers are forwarded without change and at least one non-empty transcript is sent as event" do
     ra_format = %RawAudio{channels: 1, sample_rate: 16_000, sample_format: :f32le}
 
     spec = [
@@ -43,12 +43,24 @@ defmodule Membrane.Whisper.Integration.DummyTest do
         output_stream_format: ra_format,
         serving: load_whisper_serving()
       })
-      |> child(:sink, %Membrane.File.Sink{location: @output_file})
+      |> child(:tee, Membrane.Tee)
+      |> child(:sink, %Membrane.File.Sink{location: @output_file}),
+      get_child(:tee) |> child(:testing_sink, Membrane.Testing.Sink)
     ]
 
     {:ok, _supervisor_pid, pipeline_pid} = Pipeline.start(spec: spec)
 
     assert_end_of_stream(pipeline_pid, :sink, :input, 20_000)
+
+    # Check if at least one transcript was generated
+    assert_sink_event(pipeline_pid, :testing_sink, %Membrane.Whisper.TranscriptEvent{
+      whisper_output: %{
+        text: text
+      }
+    })
+
+    assert is_bitstring(text) and String.length(text) > 0
+
     assert File.read!(@input_file) == File.read!(@output_file)
   end
 end
