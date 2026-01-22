8a954cca9b4227bc046be291adece01c9ccfb68d
Jakub Migdał <jakub.migdal@swmansion.com>
2026-01-22 12:40:10 +0100

    Modify config to support CUDA

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,13 @@
 import Config
 
-config :nx, default_backend: EXLA.Backend
+has_gpu? = System.find_executable("nvidia-smi") != nil
+platform_for_exla_cuda_client = if has_gpu?, do: :cuda, else: :host
+
+config :exla, :clients,
+  cuda: [
+    platform: platform_for_exla_cuda_client,
+    memory_fraction: 0.5
+  ],
+  default: [platform: :host]
+
+config :nx, default_backend: {EXLA.Backend, client: :cuda}
