c2543082f575563a2b9d8a999851723986583b41
Jakub Migdał <j.migdal42@gmail.com>
2026-04-28 11:44:02 +0200

    Refactor test

--- test/integration/simple_test.exs
+++ test/integration/simple_test.exs
@@ -37,9 +37,10 @@ defmodule Membrane.Whisper.Integration.SimpleTest do
   end
 
   @input_file "test/fixtures/sherlock_1min.raw"
-  @output_file "test/fixtures/output.raw"
 
+  @tag :tmp_dir
   test "audio buffers are forwarded without change and transcripts are sent as events", ctx do
+    output_file = Path.join(ctx.tmp_dir, "output.raw")
     ra_format = %Membrane.RawAudio{sample_format: :f32le, channels: 1, sample_rate: 16_000}
 
     spec = [
@@ -49,7 +50,7 @@ defmodule Membrane.Whisper.Integration.SimpleTest do
         serving: ctx.serving
       })
       |> child(:tee, Membrane.Tee)
-      |> child(:sink, %Membrane.File.Sink{location: @output_file}),
+      |> child(:sink, %Membrane.File.Sink{location: output_file}),
       get_child(:tee) |> child(:testing_sink, Membrane.Testing.Sink)
     ]
 
@@ -57,33 +58,66 @@ defmodule Membrane.Whisper.Integration.SimpleTest do
 
     assert_end_of_stream(pipeline_pid, :sink, :input, 20_000)
 
-#    [
-#      assert_sink_event(pipeline_pid, :testing_sink, %TranscriptEvent{
-#        text:
-#          " Adventure 1, a scandal in Bohemia from the adventures of Sherlock Holmes by Sir Arthur Conan Doyle.",
-#        start_timestamp_seconds: +0.0,
-#        end_timestamp_seconds: 7.0
-#      }),
-#      assert_sink_event(pipeline_pid, :testing_sink, %TranscriptEvent{
-#        text:
-#          " This is a Librevox recording. All Librevox recordings are in the public domain, for more information or to volunteer, please visit librivox.org.",
-#        start_timestamp_seconds: 7.0,
-#        end_timestamp_seconds: 19.89
-#      }),
-#      assert_sink_event(pipeline_pid, :testing_sink, %TranscriptEvent{
-#        text: " Recording by Ruth Golden. A scan. recording by rescoating, a scandal in Bohemia.",
-#        start_timestamp_seconds: 19.89,
-#        end_timestamp_seconds: 25.0
-#      }),
-#      assert_sink_event(pipeline_pid, :testing_sink, %TranscriptEvent{
-#        text:
-#          " To Sherlock Holmes, she is always the woman. I have seldom heard him mention her under any other name. In his eyes she eclipses and predominates the whole of her sex. It was not that he felt any emotion akin to love for iron-eyedler. All emotions and that one particularly were apparent to his cold, precise, but admirably balanced mind. He was, I take it.",
-#        start_timestamp_seconds: 25.0,
-#        end_timestamp_seconds: 59.89399999999998
-#      })
-#    ]
+    [
+      %TranscriptEvent{
+        text:
+          " Adventure 1, a scandal in Bohemia from the adventures of Sherlock Holmes by Sir Arthur Conan Doyle.",
+        start_timestamp_seconds: 0.0,
+        end_timestamp_seconds: 7.0
+      },
+      %TranscriptEvent{
+        text: " This is a Librevox recording.",
+        start_timestamp_seconds: 7.0,
+        end_timestamp_seconds: 10.0
+      },
+      %TranscriptEvent{
+        text: " All Librevox recordings are in the public domain.",
+        start_timestamp_seconds: 10.0,
+        end_timestamp_seconds: 13.5
+      },
+      %TranscriptEvent{
+        text: " For more information or to volunteer, please visit librevox.org.",
+        start_timestamp_seconds: 13.5,
+        end_timestamp_seconds: 19.5
+      },
+      %TranscriptEvent{
+        text: " recording by rescoating, a scandal in Bohemia.",
+        start_timestamp_seconds: 20.0,
+        end_timestamp_seconds: 25.0
+      },
+      %TranscriptEvent{
+        text: " To Sherlock Holmes, she is always the woman.",
+        start_timestamp_seconds: 25.0,
+        end_timestamp_seconds: 30.0
+      },
+      %TranscriptEvent{
+        text: " I have seldom heard him mention her under any other name.",
+        start_timestamp_seconds: 30.0,
+        end_timestamp_seconds: 35.0
+      },
+      %TranscriptEvent{
+        text: " In his eyes she eclipses and predominates the whole of her.",
+        start_timestamp_seconds: 35.0,
+        end_timestamp_seconds: 40.0
+      },
+      %TranscriptEvent{
+        text: " sex. It was not that he felt any emotion akin to love for iron-eyedler. All",
+        start_timestamp_seconds: 40.0,
+        end_timestamp_seconds: 48.24
+      },
+      %TranscriptEvent{
+        text: " emotions and that one",
+        start_timestamp_seconds: 48.24,
+        end_timestamp_seconds: 50.0
+      },
+      %TranscriptEvent{
+        text:
+          " particularly were apparent to his cold, precise, but admirably balanced mind. He was, I take it.",
+        start_timestamp_seconds: 58.0,
+        end_timestamp_seconds: 59.84
+      }
+    ]
 
-    assert File.read!(@input_file) == File.read!(@output_file)
-    File.rm!(@output_file)
+    assert File.read!(@input_file) == File.read!(output_file)
   end
 end
